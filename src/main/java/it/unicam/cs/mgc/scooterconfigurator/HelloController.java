ef059af5cf720453eddb865be15e3b9138c4e80d
turtle <alessio.tartufoli@studenti.unicam.it>
2024-12-27 18:54:52 +0100

    Initial commit

---
+++ src/main/java/it/unicam/cs/mgc/scooterconfigurator/HelloController.java
@@ -0,0 +1,14 @@
+package it.unicam.cs.mgc.scooterconfigurator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
